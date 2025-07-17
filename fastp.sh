b161e6ad89d70788ae590ff3598d7c2d9d09c9ab
braulioassis <52830010+braulioassis@users.noreply.github.com>
2025-07-17 08:57:15 -0600

    Add files via upload

---
+++ fastp.sh
@@ -0,0 +1,28 @@
+DATA_DIR=/home/kwilst/WGBS/PROCESSED
+
+source ~/miniconda3/bin/activate
+conda activate fastp
+
+cd $DATA_DIR
+
+ls /home/dio3/wilstermanlab/UM_051-19_RawSeqData/WGBS_DATA/L*_1.fq.gz  | while read file; do
+        ref=$(echo "${file}" | cut -d "_" -f 1-7) 
+        name=$(echo "${file}" | cut -d "_" -f 1-7 | cut -d "/" -f 7)
+        echo "${name}"
+        if [ -f "${name}.R1.fastp.fq.gz" ]; then
+                echo "exists; skipping..."
+        else
+               ### -g (PolyG)
+               ### -Q (disable quality filtering)
+               ### -L (disable length filtering)
+               ### -A (disable adaptor trimming)
+                fastp -g -Q -w 8\
+                	-h "${name}.fastp.html" \
+                	-i "${file}" \
+                	-I "${ref}_2.fq.gz" \
+                	-o "${name}.R1.fastp.fq.gz" \
+                	-O "${name}.R2.fastp.fq.gz"
+            
+        fi
+done
+echo "Done!"
