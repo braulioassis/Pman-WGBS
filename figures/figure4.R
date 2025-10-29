c9d63a60c88e1de6e18ecd4cbcb561ff38fe00f1
braulioassis <52830010+braulioassis@users.noreply.github.com>
2025-10-29 11:13:18 -0600

    Update figure4.R

--- figures/figure4.R
+++ figures/figure4.R
@@ -2,9 +2,9 @@ library(ggplot2)
 library(gridExtra)
 library(dplyr)
 
-cor_results <- read.csv("w.spearman.CPM.csv")
+cor_results <- read.csv("W.genebody_cor_results.csv")
 cor_results <- na.omit(cor_results)
-cor_results$bin <- cut(cor_results$rho, breaks = seq(floor(min(cor_results$rho)), ceiling(max(cor_results$rho)), by = 0.1), include.lowest = T)
+cor_results$bin <- cut(cor_results$Cor, breaks = seq(floor(min(cor_results$Cor)), ceiling(max(cor_results$Cor)), by = 0.1), include.lowest = T)
 cor_counts <- as.data.frame(table(cor_results$bin))
 colnames(cor_counts) <- c("bin", "count")
 cor_counts$midpoint <- sapply(strsplit(as.character(cor_counts$bin), ","), function(x) {
@@ -12,7 +12,7 @@ cor_counts$midpoint <- sapply(strsplit(as.character(cor_counts$bin), ","), funct
 })
 cor_counts$highlight <- with(cor_counts, ifelse(midpoint > 0.3 | midpoint < -0.3, "Highlight", "Normal"))
 
-sig_results <- filter(cor_results, abs(rho) >= 0.3, fdr < 0.05)
+sig_results <- filter(cor_results, abs(Cor) >= 0.3, FDR < 0.05)
 
 p1 <- ggplot(cor_counts, aes(x = midpoint, y = count, fill = highlight)) +
   geom_col() +
@@ -28,9 +28,9 @@ p1 <- ggplot(cor_counts, aes(x = midpoint, y = count, fill = highlight)) +
         panel.grid.minor.x = element_blank()
   )
 
-cor_results <- read.csv("lz.spearman.CPM.csv")
+cor_results <- read.csv("LZ.genebody_cor_results.csv")
 cor_results <- na.omit(cor_results)
-cor_results$bin <- cut(cor_results$rho, breaks = seq(floor(min(cor_results$rho)), ceiling(max(cor_results$rho)), by = 0.1), include.lowest = T)
+cor_results$bin <- cut(cor_results$Cor, breaks = seq(floor(min(cor_results$Cor)), ceiling(max(cor_results$Cor)), by = 0.1), include.lowest = T)
 cor_counts <- as.data.frame(table(cor_results$bin))
 colnames(cor_counts) <- c("bin", "count")
 cor_counts$midpoint <- sapply(strsplit(as.character(cor_counts$bin), ","), function(x) {
@@ -52,9 +52,9 @@ p2 <- ggplot(cor_counts, aes(x = midpoint, y = count, fill = highlight)) +
         panel.grid.minor.x = element_blank()
   )
 
-cor_results <- read.csv("jz.spearman.CPM.csv")
+cor_results <- read.csv("JZ.genebody_cor_results.csv")
 cor_results <- na.omit(cor_results)
-cor_results$bin <- cut(cor_results$rho, breaks = seq(floor(min(cor_results$rho)), ceiling(max(cor_results$rho)), by = 0.1), include.lowest = T)
+cor_results$bin <- cut(cor_results$Cor, breaks = seq(floor(min(cor_results$Cor)), ceiling(max(cor_results$Cor)), by = 0.1), include.lowest = T)
 cor_counts <- as.data.frame(table(cor_results$bin))
 colnames(cor_counts) <- c("bin", "count")
 cor_counts$midpoint <- sapply(strsplit(as.character(cor_counts$bin), ","), function(x) {
