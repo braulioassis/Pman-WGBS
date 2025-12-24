eed827fa3528b96ad650146061d3bedc0594a6a9
braulioassis <52830010+braulioassis@users.noreply.github.com>
2025-12-24 10:27:07 -0700

    Update figureS6.R

--- figures/figureS6.R
+++ figures/figureS6.R
@@ -1,9 +1,8 @@
 library(ggplot2)
 library(gridExtra)
 
+# P1
 perms <- readRDS("W-genebody-CPM-perm_hits.rds")
-hist(perms)
-
 perms <- data.frame(perms)
 perms$Hits <- perms$perms
 
@@ -15,18 +14,15 @@ p1 <- ggplot(perms, aes(x = Hits)) +
   geom_vline(xintercept = quantile(perms$Hits, 0.95), color = "firebrick3", size = 1) +
   geom_vline(xintercept = 6264, color = "seagreen3", size = 1) +
   theme_minimal() +
-  theme(plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 16),
-        axis.text = element_text(size = 14)) +
-  labs(title = "Early gestation, whole placenta",
-       x = "", y = "Count")
+  theme(plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
+        axis.title = element_text(size = 8),
+        axis.text = element_text(size = 8)) +
+  labs(x = "", y = "Count", title = "Early gestation, whole placenta")
 
+# P2
 perms <- readRDS("LZ-genebody-CPM-perm_hits.rds")
-hist(perms)
-
 perms <- data.frame(perms)
 perms$Hits <- perms$perms
-
 
 p2 <- ggplot(perms, aes(x = Hits)) +
   geom_histogram(bins = 30, 
@@ -36,15 +32,13 @@ p2 <- ggplot(perms, aes(x = Hits)) +
   geom_vline(xintercept = quantile(perms$Hits, 0.95), color = "firebrick3", size = 1) +
   geom_vline(xintercept = 4261, color = "seagreen3", size = 1) +
   theme_minimal() +
-  theme(plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 16),
-        axis.text = element_text(size = 14)) +
-  labs(title = "Late gestation, labyrinth",
-       x = "Significant methylation-expression correlations", y = "")
+  theme(plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
+        axis.title = element_text(size = 8),
+        axis.text = element_text(size = 8)) +
+  labs(x = "Permuted methylation-expression correlations", y = "", title = "Late gestation, labyrinth")
 
+# P3
 perms <- readRDS("JZ-genebody-CPM-perm_hits.rds")
-hist(perms)
-
 perms <- data.frame(perms)
 perms$Hits <- perms$perms
 
@@ -56,16 +50,16 @@ p3 <- ggplot(perms, aes(x = Hits)) +
   geom_vline(xintercept = quantile(perms$Hits, 0.95), color = "firebrick3", size = 1) +
   geom_vline(xintercept = 2367, color = "seagreen3", size = 1) +
   theme_minimal() +
-  theme(plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 16),
-        axis.text = element_text(size = 14)) +
-  labs(title = "Late gestation, junctional zone",
-       x = "", y = "")
+  theme(plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
+        axis.title = element_text(size = 8),
+        axis.text = element_text(size = 8)) +
+  labs(x = "", y = "", title = "Late gestation, junctional zone")
 
-png("FigS6.png", res = 300, width = 15, height = 5, units = "in")
+# Render
+png("Figure S6.png", res = 300, width = 8.5, height = 2.8, units = "in")
 grid.arrange(p1, p2, p3, nrow = 1)
 dev.off()
 
-pdf("FigS6.pdf", width = 15, height = 5)
+pdf("Figure S6.pdf", width = 8.5, height = 2.8)
 grid.arrange(p1, p2, p3, nrow = 1)
 dev.off()
